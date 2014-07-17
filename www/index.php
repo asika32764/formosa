84a2b81fd14d37352912d77f3e1f81b8139058d8
Simon Asika <asika32764@gmail.com>
2014-07-17 21:16:13 +0800

    Move auth data to here

--- www/index.php
+++ www/index.php
@@ -6,14 +6,21 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-define('FORMOSA_ROOT',     realpath(__DIR__ . '/..'));
-define('FORMOSA_WWW',      __DIR__);
-define('FORMOSA_TEMPLATE', FORMOSA_ROOT . '/templates');
-define('FORMOSA_SOURCE',   FORMOSA_ROOT . '/src');
-define('FORMOSA_ETC',      FORMOSA_ROOT . '/etc');
+define('FORMOSA_ROOT',      realpath(__DIR__ . '/..'));
+define('FORMOSA_WWW',       __DIR__);
+define('FORMOSA_TEMPLATE',  FORMOSA_ROOT . '/templates');
+define('FORMOSA_SOURCE',    FORMOSA_ROOT . '/src');
+define('FORMOSA_RESOURCES', FORMOSA_ROOT . '/resources');
+define('FORMOSA_ETC',       FORMOSA_ROOT . '/etc');
 
 define('JPATH_ROOT',       FORMOSA_ROOT);
 
 include __DIR__ . '/../vendor/autoload.php';
 
-(new \Formosa\Application\Application)->execute();
+$config = new \Joomla\Registry\Registry;
+
+$config->loadFile(FORMOSA_ETC . '/config.yml', 'yaml');
+
+$input = new \Joomla\Input\Input;
+
+(new \Formosa\Application\Application($input, $config))->execute();
