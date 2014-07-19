bf8576259a16e101ba38d04279b0a6f73a58b015
Simon Asika <asika32764@gmail.com>
2014-07-19 22:25:25 +0800

    Update for speed

---
+++ bin/benchmark.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Part of auth project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+$start_time = microtime(TRUE);
+
+$i = 1;
+$second = 5;
+
+while (true)
+{
+	file_get_contents($argv[1] . '/www');
+
+	echo '.';
+
+	$current_time = microtime(TRUE);
+
+	if (($current_time - $start_time) >= $second)
+	{
+		echo sprintf("%s times in %s seconds.", $i, $second) . "\n\n";
+
+		die;
+	}
+
+	$i++;
+}
+
+$end_time = microtime(TRUE);
+
+echo "\n\n" . $end_time - $start_time . "\n";
